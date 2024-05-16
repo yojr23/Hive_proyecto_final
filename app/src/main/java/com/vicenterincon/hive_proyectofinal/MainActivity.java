6b765d46acea833550a644a7978525099a993579
yojr23 <79813101+yojr23@users.noreply.github.com>
2024-05-16 05:33:09 -0500

    menu navigation done - waiting for more screens anda database connection

--- app/src/main/java/com/vicenterincon/hive_proyectofinal/MainActivity.java
+++ app/src/main/java/com/vicenterincon/hive_proyectofinal/MainActivity.java
@@ -1,14 +1,61 @@
 package com.vicenterincon.hive_proyectofinal;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-
+import android.view.Menu;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.vicenterincon.hive_proyectofinal.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Block the screen rotation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomePageFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.ic_home:
+                    replaceFragment(new HomePageFragment());
+                    return true;
+                case R.id.ic_create:
+                    replaceFragment(new HomePageFragment());
+                    return true;
+                case R.id.ic_calendar:
+                    replaceFragment(new HomePageFragment());
+                    return true;
+                case R.id.ic_profile:
+                    replaceFragment(new HomePageFragment());
+                    return true;
+                default:
+                    return false;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
