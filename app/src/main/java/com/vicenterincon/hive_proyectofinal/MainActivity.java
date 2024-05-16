46fd725572e2cfaaa72910c0579b3b588b68d058
Daniel Aguilera <79813101+yojr23@users.noreply.github.com>
2024-05-16 04:42:15 -0500

    upload and update gradle. Some variables were missing

--- app/src/main/java/com/vicenterincon/hive_proyectofinal/MainActivity.java
+++ app/src/main/java/com/vicenterincon/hive_proyectofinal/MainActivity.java
@@ -1,8 +1,8 @@
 package com.vicenterincon.hive_proyectofinal;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
